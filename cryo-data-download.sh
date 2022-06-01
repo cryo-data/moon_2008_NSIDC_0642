8778a2e47ed3d0c79d69025e9e73953da23871f8
Kenneth D. Mankoff <mankoff@gmail.com>
2022-06-01 08:18:12 -0700

    cryo-data meta and download

--- cryo-data-download.sh
+++ cryo-data-download.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-root=https://n5eil01u.ecs.nsidc.org/MEASURES/NSIDC-0642.001/2000.09.30
-file=GlacierIDs_v01.2
-for ext in cpg dbf prj sbn sbx shp shx xml; do
-  datalad download-url ${root}/${file}.${ext}
+wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies --no-check-certificate --auth-no-challenge=on -r -np -e robots=off --spider --force-html -nH --cut-dirs=2 https://n5eil01u.ecs.nsidc.org/MEASURES/NSIDC-0642.001/ > stdout.txt 2> stderr.txt
+
+echo "url,file" > urls.txt
+for url in $(grep '^--' stderr.txt | awk '{ print $3 }' | grep -v '\(\/?\|\/$\)' | sort | uniq); do
+  echo $url,$(echo $url| cut -d"/" -f6-) >> urls.txt
 done
 
+# --dry-run
+datalad addurls --nosave --fast urls.txt '{url}' '{file}'
+
+rm stderr.txt stdout.txt urls.txt
